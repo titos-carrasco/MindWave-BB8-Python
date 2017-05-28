e68d68ef5cebd9dca698d5529ee78f3061b52b9f
Roberto <roberto@TitosNotebook.titos.home>
2017-05-28 19:18:49 -0400

    Corrige demo con mindwave

--- BB8-MindWave-Test.py
+++ BB8-MindWave-Test.py
@@ -4,13 +4,14 @@
 
 from rcr.utils import Utils
 from rcr.SpheroBB8 import SpheroBB8
+from rcr.mindwave import MindWave
 
 bb8 = SpheroBB8.Sphero( 'FA:8B:91:F4:9D:22' )
 
-mw = MindWave( "/dev/ttyUSB0", 1000, 0X00, 0X00 )
+mw = MindWave.MindWave( "/dev/ttyUSB0", 1000, 0X00, 0X00 )
 if( mw.connect() ):
     bb8.setRGBLedOutput( 0, 0, 0 )
-    for i in range( 1000 ):
+    for i in range( 10000 ):
         mwd = mw.getMindWaveData()
         print vars(mwd)
         if( mwd.attentionESense >=90 ):
@@ -18,10 +19,10 @@ if( mw.connect() ):
         elif( mwd.attentionESense >=70 ):
             bb8.setRGBLedOutput( 0, 255, 0 )
         elif( mwd.attentionESense >=50 ):
-            bb8.setRGBLedOutput( 0, 0, 255, 0 )
+            bb8.setRGBLedOutput( 0, 0, 255 )
         #else:
         #    bb8.setRGBLedOutput( 0, 0, 0, 0, False )
-        bb8.ping()
+        #bb8.ping()
         Utils.pause( 10 )
     mw.disconnect()
 
